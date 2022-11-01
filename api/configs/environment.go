2bc6a079b29ac96ecc247f8e6064409fd4b472f4
Pedro Andres Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2022-11-01 14:09:30 -0500

    Reorganize environment variables

--- api/configs/environment.go
+++ api/configs/environment.go
@@ -6,28 +6,46 @@ import(
   "github.com/joho/godotenv"
 )
 
+func InitEnv(){
+  // Try to get environment variables from system
+  var done = true 
+  var secrets = []string{ os.Getenv("PG_USER"), os.Getenv("PG_PASSWORD"), os.Getenv("PG_HOST"), os.Getenv("PG_PORT"), os.Getenv("MONGO_USER"), os.Getenv("MONGO_PASSWORD"), os.Getenv("MONGO_HOST"), os.Getenv("MONGO_PORT"), os.Getenv("JWT_KEY") }
+
+  // Check if all the required variables were loaded
+  for _, value := range(secrets) {
+    if value == ""{
+      done = false
+      err := godotenv.Load()
+
+      if(err != nil){
+	panic("🟥 Unable to load environment variables FROM .env FILE 🟥")
+      }
+
+      break
+    }
+  }
+  
+  if !done {
+    // Reload secrets
+    secrets = []string{ os.Getenv("PG_USER"), os.Getenv("PG_PASSWORD"), os.Getenv("PG_HOST"), os.Getenv("PG_PORT"), os.Getenv("MONGO_USER"), os.Getenv("MONGO_PASSWORD"), os.Getenv("MONGO_HOST"), os.Getenv("MONGO_PORT"), os.Getenv("JWT_KEY") }
+
+    for _, value := range(secrets) {
+      if value == ""{
+	panic("🟥 Unable to load ALL the required environment variables 🟥")
+      }
+    }
+  }
+
+}
+
 // getPostgresURI return postgres URI created from environment variables
 func getPostgresURI() string{
-  // Try to get environment variables from system
+  InitEnv()
+
   user := os.Getenv("PG_USER")
   password := os.Getenv("PG_PASSWORD")
   host := os.Getenv("PG_HOST")
   port := os.Getenv("PG_PORT")
-
-  // Gen environment variables from .env file if needed
-  if user == "" || password == "" || host == "" || port == "" {
-    err := godotenv.Load()
-
-    if err != nil {
-      panic("🟥 Unable to load environment variables 🟥")
-    }
-
-    user = os.Getenv("PG_USER")
-    password = os.Getenv("PG_PASSWORD")
-    host = os.Getenv("PG_HOST")
-    port = os.Getenv("PG_PORT")
-
-  }
 
   URI := fmt.Sprintf("postgres://%s:%s@%s:%s/buenavida?sslmode=disable", user, password, host, port)
   return URI
@@ -36,26 +54,12 @@ func getPostgresURI() string{
 
 // getMongoURI return mongo URI created from environment variables
 func getMongoURI() string {
-  // Try to get environment variables from system
+  InitEnv() 
+
   user := os.Getenv("MONGO_USER")
   password := os.Getenv("MONGO_PASSWORD")
   host := os.Getenv("MONGO_HOST")
   port := os.Getenv("MONGO_PORT")
-
-  // Get environment variables from .env file if needed
-  if user == "" || password == "" || host == "" || port == "" {
-    // Load environment variables from .env file
-    err := godotenv.Load()
-
-    if err != nil {
-      panic("🟥 Unable to load environment variables 🟥")
-    }
-    
-    user = os.Getenv("MONGO_USER")
-    password = os.Getenv("MONGO_PASSWORD")
-    host = os.Getenv("MONGO_HOST")
-    port = os.Getenv("MONGO_PORT")
-  }
   
   // Generate and return URI
   URI := fmt.Sprintf("mongodb://%s:%s@%s:%s", user, password, host, port)
--- api/go.mod
+++ api/go.mod
@@ -17,6 +17,7 @@ require (
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
