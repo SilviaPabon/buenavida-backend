ba398b580f33b637eee5e3d15cb51d1dc81561ce
Pedro Andres Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2022-11-01 12:43:43 -0500

    Validate non empty fields

--- api/controllers/session.go
+++ api/controllers/session.go
@@ -7,6 +7,25 @@ import(
 )
 
 func HandleLogin(c echo.Context) error {
+  // Get json payload
+  var payload = new(interfaces.LoginPayload)
+  err := c.Bind(payload)
+
+  if err != nil {
+    return c.JSON(http.StatusBadRequest, interfaces.GenericResponse{
+      Error: true, 
+      Message: "Unable to process query. Try again and make sure mail and password fields were provided",
+    })
+  }
+
+  // Validate json payload
+  if payload.Mail == "" || payload.Password == "" {
+    return c.JSON(http.StatusBadRequest, interfaces.GenericResponse{
+      Error: true, 
+      Message: "Mail and password files are required / can't be empty",
+    })
+  }
+
   return c.JSON(http.StatusOK, interfaces.GenericResponse{
     Error: false, 
     Message: "Hello world",
--- api/interfaces/requests.go
+++ api/interfaces/requests.go
@@ -3,3 +3,8 @@ package interfaces
 type FilterProductsByText struct{
   Criteria	string		`json:"search-criteria"`
 }
+
+type LoginPayload struct{
+  Mail		string 		`json:"mail"`
+  Password	string		`json:"password"`
+}
