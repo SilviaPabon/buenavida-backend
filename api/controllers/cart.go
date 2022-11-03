8e098efa991952024863eaf4c1f8aeb4f154051d
Pedro Andres Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2022-11-03 07:21:14 -0500

    Update cart table (model)

--- api/controllers/cart.go
+++ api/controllers/cart.go
@@ -1,9 +1,12 @@
 package controllers
 
 import(
-  //"fmt"
+  // "fmt"
   "net/http"
   "github.com/labstack/echo/v4"
+  "github.com/golang-jwt/jwt/v4"
+  "github.com/SilviaPabon/buenavida-backend/configs"
+  "github.com/SilviaPabon/buenavida-backend/models"
   "github.com/SilviaPabon/buenavida-backend/interfaces"
 )
 
@@ -27,9 +30,35 @@ func HandleCartPost(c echo.Context) error {
   }
 
   // Validate the product exists on mongo
+  _, err := models.GetDetailsFromID(payload.Id.Hex())
+
+  if err != nil {
+    return c.JSON(http.StatusNotFound, interfaces.GenericResponse{
+      Error: true, 
+      Message: "Product was not found.",
+    })
+  }
+
+  // Get user id from token
+  cookie, _ := c.Cookie("access-token")
+  token := cookie.Value
+  claims := &interfaces.JWTCustomClaims{}
+  jwt.ParseWithClaims(token, claims, func(t *jwt.Token) (interface{}, error){
+    return configs.GetJWTSecret(), nil
+  })
+
+  // Save on database
+  err = models.AddProductToCart(claims.ID, payload.Id.Hex())
+
+  if err != nil {
+    return c.JSON(http.StatusInternalServerError, interfaces.GenericResponse{
+      Error: true, 
+      Message: "Unable to add the product to the user cart. Try again.",
+    })
+  }
 
   return c.JSON(http.StatusOK, interfaces.GenericResponse{
     Error: false, 
-    Message: "OK",
+    Message: "Product added to the cart successfully",
   })
 }
---
+++ api/models/cart.go
@@ -0,0 +1,46 @@
+package models
+
+import(
+  // "fmt"
+  "context"
+  "time"
+  "github.com/SilviaPabon/buenavida-backend/configs"
+  // "github.com/SilviaPabon/buenavida-backend/interfaces"
+)
+
+// Mongodb collections
+var pg = configs.ConnectToPostgres()
+
+// AddProductToCart Add a new product to the cart or update the current ammount
+func AddProductToCart(userId int, productId string) error {
+  ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+  defer cancel()
+
+  // ### ### ###
+  // Verify if the product already exists
+  query := `SELECT COUNT("idUser") AS count FROM cart WHERE
+	    "idUser" = $1 AND "idArticle" = $2;`
+  
+  row := pg.QueryRowContext(ctx, query, userId, productId)
+  var count int
+  err := row.Scan(&count)
+
+  if err != nil{
+    return err
+  }
+
+  // ### #### ### Insert or update as needed
+  if count == 0 {
+    query = `INSERT INTO cart ("idUser", "idArticle", "amount")
+	      VALUES ($1, $2, $3);` 
+    row = pg.QueryRowContext(ctx, query, userId, productId, 1)
+    return row.Err() // Returns nil or error if some error exists
+  }else{
+    query = `UPDATE cart 
+	    SET "amount" = "amount" + 1
+	    WHERE "idUser" = $1 AND "idArticle" = $2`
+    row = pg.QueryRowContext(ctx, query, userId, productId)
+    return row.Err()
+  }
+
+}
