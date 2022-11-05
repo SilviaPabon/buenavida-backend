20bd064f1edf0a640a775905ef47d63ca0e24332
Pedro Andres Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2022-11-04 19:23:35 -0500

    Update orders table and relations

--- database/init.sql
+++ database/init.sql
@@ -24,23 +24,6 @@ CREATE TABLE IF NOT EXISTS users
     password VARCHAR ( 250 ) NOT NULL
 );
 
--- DROP TABLE IF EXISTS orders;
-
-CREATE TABLE IF NOT EXISTS orders
-(
-    "idOrder" SERIAL NOT NULL PRIMARY KEY,
-    "idUser" INTEGER NOT NULL,
-    "orderDetails" JSON NOT NULL,
-    -- 5 Numbers, 2 decimals
-    total NUMERIC (5, 2) NOT NULL CHECK (total > 0),
-    discount NUMERIC (5, 2) NOT NULL CHECK (discount > 0),
-    CONSTRAINT fk_orders_users
-	FOREIGN KEY ("idUser")
-	    REFERENCES users (id)
-	    ON UPDATE CASCADE
-	    ON DELETE CASCADE
-);
-
 -- DROP TABLE IF EXISTS "shoppingCart";
 
 CREATE TABLE IF NOT EXISTS "cart"
@@ -55,6 +38,36 @@ CREATE TABLE IF NOT EXISTS "cart"
 	    ON DELETE CASCADE,
     CONSTRAINT cart_check_product_id
 	check ("idArticle" ~ '^[0-9a-fA-F]{24}$')
+);
+
+-- DROP TABLE IF EXISTS "orders"
+CREATE TABLE IF NOT EXISTS orders
+(
+  "idOrder" SERIAL NOT NULL PRIMARY KEY, 
+  "idUser" INTEGER NOT NULL,
+  "date" TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+  CONSTRAINT fk_orders_users
+    FOREIGN KEY ("idUser")
+      REFERENCES users(id)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE
+);
+
+-- DROP TABLE IF EXISTS "orders_has_products"
+CREATE TABLE IF NOT EXISTS orders_has_products
+(
+  "idOrder" INTEGER NOT NULL, 
+  "idArticle" CHAR ( 24 ) NOT NULL, 
+  "amount" SMALLINT NOT NULL CHECK (amount > 0), 
+  "price" NUMERIC (5,2) NOT NULL CHECK (price > 0), 
+  "discount" NUMERIC (5,2) NOT NULL CHECK (discount >= 0),
+  CONSTRAINT order_has_products_product_id
+    check ("idArticle" ~ '^[0-9a-fA-F]{24}$'), 
+  CONSTRAINT order_has_products_order_id
+    FOREIGN KEY ("idOrder")
+      REFERENCES orders ("idOrder")
+      ON UPDATE CASCADE
+      ON DELETE CASCADE
 );
 
 -- DROP TABLE IF EXISTS favorites;
