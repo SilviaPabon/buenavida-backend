0c1a45a01d9b4aab2734ecef73d680fbe8fdeafc
Pedro Andres Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2022-11-08 06:46:06 -0500

    🍪 Send expired and empty cookies

--- api/controllers/session.go
+++ api/controllers/session.go
@@ -193,8 +193,50 @@ func HandleRefresh(c echo.Context) error {
 
 // Send expired tokens and remove refresh token from redis
 func HandleLogout(c echo.Context) error {
-  return c.JSON(http.StatusOK, interfaces.GenericResponse{
-    Error: false, 
-    Message: "Route created",
-  })
+	// *** Get token from cookie
+	cookie, _ := c.Cookie("access-token")
+	token := cookie.Value
+	claims := &interfaces.JWTCustomClaims{}
+
+	jwt.ParseWithClaims(token, claims, func(t *jwt.Token) (interface{}, error) {
+		return configs.GetJWTSecret(), nil
+	})
+
+	user := interfaces.User{
+		ID:    claims.ID,
+		Email: claims.Email,
+	}
+
+	// *** Remove refresh token from redis
+	err := models.DeleteRefreshTokenFromRedis(user.Email)
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, interfaces.GenericResponse{
+			Error:   true,
+			Message: "Unable to perform redis operation",
+		})
+	}
+
+	// *** Send tokens as cookies
+	accessCookie := new(http.Cookie)
+	accessCookie.Name = "access-token"
+	accessCookie.Value = "" // Empty string 
+	accessCookie.Expires = time.Now().Add(2 * time.Second)
+	accessCookie.HttpOnly = true
+	accessCookie.Path = "/"
+
+	refreshCookie := new(http.Cookie)
+	refreshCookie.Name = "refresh-token"
+	refreshCookie.Value = "" // Empty string
+	refreshCookie.Expires = time.Now().Add(2 * time.Second)
+	refreshCookie.HttpOnly = true
+	refreshCookie.Path = "/api/session/refresh"
+
+	c.SetCookie(accessCookie)
+	c.SetCookie(refreshCookie)
+
+	return c.JSON(http.StatusOK, interfaces.GenericResponse{
+		Error:   false,
+		Message: "Successfully logged out",
+	})
 }
--- api/models/session.go
+++ api/models/session.go
@@ -36,3 +36,12 @@ func GetRefreshTokenFromRedis(owner string) (string, error) {
 	token, err := redis.Get(ctx, owner).Result()
 	return token, err
 }
+
+// DeleteRegreshTokenFromRedis Deletes a token from the user email
+func DeleteRefreshTokenFromRedis(owner string) (error) {
+  ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+  defer cancel()
+
+  err := redis.Del(ctx, owner).Err()
+  return err
+}
